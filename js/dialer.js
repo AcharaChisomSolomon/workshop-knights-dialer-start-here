01af028a9a08c0ecb51c6504860023a7731a98be
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-12 16:45:33 +0100

    chore: Add knightMoves array and implement reachableKeys, countPaths, and listAcyclicPaths functions

--- js/dialer.js
+++ js/dialer.js
@@ -6,34 +6,55 @@ export default {
 
 
 // ****************************
+const knightMoves = [
+	[4, 6],
+	[6, 8],
+	[7, 9],
+	[4, 8],
+	[3, 9, 0],
+	[],
+	[1, 7, 0],
+	[2, 6],
+	[1, 3],
+	[2, 4]
+];
 
 function reachableKeys(startingDigit) {
-	// TODO: return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-	return [];
+	return knightMoves[startingDigit];
 }
 
-function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+function countPaths(startingDigit,hopCount, paths = [], visited = []) {
+	visited.push(startingDigit);
+
+	const reachable = knightMoves[startingDigit];
+
+	if (hopCount === 0) {
+		paths.push(visited);
+		console.log(visited);
+		return
+	}
+
+	reachable.forEach(digit => {
+		countPaths(digit, hopCount - 1, paths, visited.slice());
+	});
+
+	return paths.length;
 }
 
-function listAcyclicPaths(startingDigit) {
-	// TODO: given the digit/key to start from,
-	// return a list of the distinct acyclic
-	// paths that are possible to traverse
-	//
-	// e.g. [
-	//   [4, 3, 8, 1, 6, 7, 2, 9],
-	//   [4, 3, 8, 1, 6, 0],
-	//   ...
-	// ]
-	return [];
+function listAcyclicPaths(startingDigit, visited = [], paths = []) {
+	visited.push(startingDigit);
+
+	const reachable = knightMoves[startingDigit];
+	const unvisited = reachable.filter(digit => !visited.includes(digit));
+
+	if (unvisited.length === 0) {
+		paths.push(visited);
+		return paths;
+	}
+
+	unvisited.forEach(digit => {
+		listAcyclicPaths(digit, visited.slice(), paths);
+	});
+
+	return paths;
 }
